9e03f98e16fc04c915e04ee2a5bf0e136133e4f5
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 17:28:48 -0600

    Add files via upload

---
+++ Unidad#3 Semana#3 Clase#8 Ejercicio#3.java
@@ -0,0 +1,13 @@
+package javaapplication17;
+import java.util.Scanner;
+public class JavaApplication17 {
+public static void main(String[] args) {
+    Scanner Reader= new Scanner (System.in);
+    String quieroJugar="si";
+    while (quieroJugar.equals("si")){
+        System.out.println("¿Quieres seguir jugando?");
+        quieroJugar = Reader.next();
+    }
+    }
+    
+}
