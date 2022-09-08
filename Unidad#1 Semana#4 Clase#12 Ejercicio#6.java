6b280397c9830ccbe0dc95c07b66e641c96037a7
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 16:00:18 -0600

    Add files via upload

---
+++ Unidad#1 Semana#4 Clase#12 Ejercicio#6.java
@@ -0,0 +1,9 @@
+package javaapplication6;
+public class JavaApplication6 {
+    public static void main(String[] args) {
+      double raiz= Math.sqrt (9);
+      
+      System.out.println(raiz);
+    }
+    
+}
