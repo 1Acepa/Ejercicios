6b138359fa16481f77df13f204a77e00df44c3b6
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 16:30:05 -0600

    Add files via upload

---
+++ Unidad#2 Semana#3 Clase#9 Ejercicio#3.java
@@ -0,0 +1,8 @@
+package javaapplication10;
+public class JavaApplication10 {
+    public static void main(String[] args) {
+       for (int i=100; i>= 1; i--)
+           System.out.println(i);
+    }
+    
+}
