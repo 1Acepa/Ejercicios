0f19d25c031f10ba798c4556b6aa6357314586ad
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 15:56:07 -0600

    Add files via upload

---
+++ Unidad#1 Semana#4 Clase#12 Ejercicio#5.java
@@ -0,0 +1,11 @@
+
+package javaapplication5;
+public class JavaApplication5 {
+    public static void main(String[] args) {
+       double base =5, exponente =2;
+       double resultado= Math.pow(base,exponente);
+       
+       System.out.println(resultado);
+    }
+    
+}
