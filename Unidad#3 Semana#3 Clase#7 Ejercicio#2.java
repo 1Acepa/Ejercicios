284fde36c8fedf5651b4163574c91a4e708c6c23
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 17:23:56 -0600

    Add files via upload

---
+++ Unidad#3 Semana#3 Clase#7 Ejercicio#2.java
@@ -0,0 +1,12 @@
+package javaapplication16;
+public class JavaApplication16 {
+public static void main(String[] args) {
+       int i=0;
+        while (true){ // condición trivial: siempre cierta
+            i++;
+            System.out.println ("Valor de i:" + i);
+            if (i==9) {break;}
+        }
+    }
+    
+}
