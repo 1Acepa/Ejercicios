d5ee10726cdfd1777fff9614b98f098763892552
1Acepa <113138348+1Acepa@users.noreply.github.com>
2022-09-08 17:08:14 -0600

    Add files via upload

---
+++ Unidad#2 Semana#4 Clase#12 Ejercicio#7.java
@@ -0,0 +1,11 @@
+package javaapplication14;
+public class JavaApplication14 {
+public static void main(String[] args) {
+        int suma= 0;
+        for (int i=100; i<= 200; i++) {
+            suma= suma + i;
+        }
+        System.out.println("la suma es:"+ suma);
+    }
+    
+}
